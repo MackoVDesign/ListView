4dce90b751c7e642c361218dd4a60c98e2cccf80
Trzmiel.Maciej <Trzmiel.Maciej@STACJA105-09>
2024-02-28 14:09:00 +0100

    Dodaj pliki projektów.

---
+++ WPFDialogi/ListOfPersons.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFDialogi
+{
+    internal class ListOfPersons
+    {
+        public ObservableCollection<Person> persons;
+        public ListOfPersons()
+        {
+            persons = new ObservableCollection<Person>();
+            LoadPersons("plik.txt");
+        }
+        public void AddPerson(Person person)
+        {
+            persons.Add(person);
+        }
+        public void RemovePerson(Person person)
+        {
+            persons.Remove(person);
+        }
+        public void EditPerson(int index, Person person)
+        {
+            persons[index] = person;
+        }
+        public void RemovePersonAt(int index)
+        {
+            if (index >= 0 && index < persons.Count)
+                persons.RemoveAt(index);
+        }
+        public void LoadPersons(string file)
+        {
+            persons.Clear();
+            persons.Add(new Person("Karol", "Jański", EducationLevel.średnie));
+            persons.Add(new Person("Ania", "Dudek", EducationLevel.podstawowe));
+            persons.Add(new Person("Maciej", "Król", EducationLevel.wyższe));
+            persons.Add(new Person("Marta", "Sobieski", EducationLevel.średnie));
+            persons.Add(new Person("Oskar", "MItło", EducationLevel.średnie));
+            persons.Add(new Person("Monika", "Eker", EducationLevel.podstawowe));
+            persons.Add(new Person("Dawid", "Oki", EducationLevel.wyższe));
+            persons.Add(new Person("Olgiert", "Piłka", EducationLevel.średnie));
+        }
+        public void SavePersons(string file)
+        {
+            //tu zapis do pliku
+        }
+    }
+}
---
+++ WPFDialogi/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace WPFDialogi
+{
+    public partial class MainWindow : Window
+    {
+        private ListOfPersons pList = new ListOfPersons();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            pListView.ItemsSource = pList.persons;
+        }
+
+        private void AddPerson_Click(object sender, RoutedEventArgs e)
+        {
+            PersonWindow personWindow = new PersonWindow();
+            personWindow.person = new Person();
+            personWindow.DataContext = personWindow.person;
+            if (personWindow.ShowDialog() == true)
+            {
+                pList.AddPerson(personWindow.person);
+            }
+        }
+
+        private void EditPerson_Click(object sender, RoutedEventArgs e)
+        {
+            Person per = pListView.SelectedItem as Person;
+            if (per != null)
+            {
+                PersonWindow personWindow = new PersonWindow();
+                personWindow.person = new Person(per.FirstName, per.LastName, per.Education);
+                personWindow.DataContext = personWindow.person;
+                if (personWindow.ShowDialog() == true)
+                {
+                    pList.EditPerson(pList.persons.IndexOf(per), personWindow.person);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Wybierz osobę do edycji.", "Błąd", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void DeletePerson_Click(object sender, RoutedEventArgs e)
+        {
+            Person per = pListView.SelectedItem as Person;
+            if (per != null)
+            {
+                pList.RemovePerson(per);
+            }
+            else
+            {
+                MessageBox.Show("Wybierz osobę do usunięcia.", "Błąd", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void Sort(string sortBy)
+        {
+            ICollectionView dataView = CollectionViewSource.GetDefaultView(pListView.ItemsSource);
+            dataView.SortDescriptions.Clear();
+            dataView.SortDescriptions.Add(new SortDescription(sortBy, ListSortDirection.Ascending));
+            dataView.Refresh();
+        }
+
+        private void SortByName_Click(object sender, RoutedEventArgs e)
+        {
+            Sort("FirstName");
+        }
+
+        private void SortByLastName_Click(object sender, RoutedEventArgs e)
+        {
+            Sort("LastName");
+        }
+
+        private void SortByEducation_Click(object sender, RoutedEventArgs e)
+        {
+            Sort("Education");
+        }
+    }
+}
